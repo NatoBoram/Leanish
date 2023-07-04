43ad8df54a415bc6beb887bf46ad6495965552fe
Nato Boram <nato.boram@gmail.com>
2023-07-04 16:32:04 -0400

    🔧 Prepare adapter static

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,19 @@
-import adapter from '@sveltejs/adapter-auto'
+import adapterAuto from '@sveltejs/adapter-auto'
+import adapterStatic from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
+
+/** @returns {import('@sveltejs/kit').Adapter} */
+function adapter() {
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-return
+	if (process.env.GITHUB_ACTIONS) return adapterStatic({ fallback: '404.html' })
+	return adapterAuto()
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: [vitePreprocess()],
 
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
 	},
 }
