b1f229c9f14f34bb7a57130be3b2e20c0161c3a5
Nato Boram <nato.boram@gmail.com>
2023-07-07 22:12:09 -0400

    ✨ Stop using proxy on most instances

--- src/lib/utils/cors.ts
+++ src/lib/utils/cors.ts
@@ -1,8 +1,18 @@
 import { base } from '$app/paths'
 
+/** If a Lemmy instance is blocking web clients, use a proxy. */
+const bypass = ['lemmy.world']
+
 export function cors(f: typeof fetch, origin: string): typeof fetch {
 	return (input: RequestInfo | URL, init?: RequestInit | undefined): Promise<Response> => {
 		const url = new URL('/cors', `${origin}${base}`)
+
+		if (typeof input === 'string' && bypass.every(site => site !== new URL(input).hostname))
+			return f(input, init)
+		else if (input instanceof URL && bypass.every(site => site !== input.hostname))
+			return f(input, init)
+		else if (input instanceof Request && bypass.every(site => site !== new URL(input.url).hostname))
+			return f(input, init)
 
 		if (typeof input === 'string') url.searchParams.set('url', input)
 		else if (input instanceof URL) url.searchParams.set('url', input.toString())
