ee1502665590ad4790ff0e2c277106afaeb7195b
Nato Boram <nato.boram@gmail.com>
2023-07-13 20:15:49 -0400

    🚸 Change page when changing limit
    Closes #25

--- src/lib/LimitSelector.svelte
+++ src/lib/LimitSelector.svelte
@@ -15,14 +15,23 @@
 	}
 
 	async function changeLimit(url: URL) {
-		let value = Number(input.value)
-		if (!value || isNaN(value)) value = 10
-		value = Math.min(Math.max(value, 1), 50)
+		let newLimit = Number(input.value)
+		if (!newLimit || isNaN(newLimit)) newLimit = 10
+		newLimit = Math.min(Math.max(newLimit, 1), 50)
 
-		url.searchParams.set('limit', String(value))
+		// Change the page to keep the middle post within view
+		const page = Number(url.searchParams.get('page') ?? 1)
+		const oldLimit = Number(url.searchParams.get('limit') ?? 10)
+		if (!isNaN(page) && !isNaN(oldLimit)) {
+			const targetItem = oldLimit * (page - 1) + oldLimit / 2
+			const targetPage = Math.max(Math.floor(targetItem / newLimit), 1)
+			url.searchParams.set('page', String(targetPage))
+		}
+
+		url.searchParams.set('limit', String(newLimit))
 		await goto(url.toString(), { noScroll: true })
 		await invalidate('app:paginate')
-		dispatch('limit', value)
+		dispatch('limit', newLimit)
 	}
 </script>
 
