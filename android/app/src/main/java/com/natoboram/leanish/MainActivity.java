08c9a65fa3ec52f71ff014cdefa60f18722a5744
Nato Boram <nato.boram@gmail.com>
2023-08-06 23:48:51 -0400

    ✨ Start building with Gradle instead of capacitor

--- android/app/src/main/java/com/natoboram/leanish/MainActivity.java
+++ android/app/src/main/java/com/natoboram/leanish/MainActivity.java
@@ -2,4 +2,5 @@ package com.natoboram.leanish;
 
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+}
