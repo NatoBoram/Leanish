be4eae77ace131c3f85d627b6425414dbb4ee56f
Nato Boram <nato.boram@gmail.com>
2023-08-08 13:21:57 -0400

    ⬆️ Update lemmy-js-client

--- android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
+++ android/app/src/androidTest/java/com/getcapacitor/myapp/ExampleInstrumentedTest.java
@@ -3,10 +3,8 @@ package com.getcapacitor.myapp;
 import static org.junit.Assert.assertEquals;
 
 import android.content.Context;
-
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
