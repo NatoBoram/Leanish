92e9ac6fba2823dae6b25a3f7aaefb8ad797728b
Nato Boram <nato.boram@gmail.com>
2023-08-10 20:50:13 -0400

    💡 Make comment_tree more readable

--- src/lib/comments/comment_tree.ts
+++ src/lib/comments/comment_tree.ts
@@ -7,18 +7,24 @@ export function buildCommentTree(commentViews: CommentView[], parentId: CommentI
 
 	// flat_path = "0.123.456"
 	// node_path = "0.123"
-	// if (flat_path = "node_path.flat_id") node.push(flat)
+	// if (flat_path === `${node_path}.${flat_id}`) node.push(flat)
 	for (const flat of nodes) {
-		const node = nodes.find(
-			node =>
-				`${node.comment.comment.path}.${flat.comment.comment.id}` === flat.comment.comment.path,
-		)
+		const node = nodes.find(node => {
+			const { id: flatId, path: flatPath } = flat.comment.comment
+			const { path: nodePath } = node.comment.comment
+
+			// "{0.123}.{456}" === "{0.123.456}"
+			return `${nodePath}.${flatId}` === flatPath
+		})
 
 		if (node) node.children.push(flat)
 		else if (
-			parentId === undefined ||
+			// Top-level comments are always in the root of the tree.
 			flat.comment.comment.path === `0.${flat.comment.comment.id}` ||
-			flat.comment.comment.path.endsWith(`.${parentId}`)
+			// The parent comment itself should be the only root comment.
+			flat.comment.comment.path.endsWith(`.${parentId}`) ||
+			// Comments are either top-level or have a parent... except in search results.
+			parentId === undefined
 		)
 			tree.push(flat)
 	}
