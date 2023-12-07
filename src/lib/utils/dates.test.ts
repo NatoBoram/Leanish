06ef77548c7decaf9b1f993411d4a7dedadb7065
Nato Boram <NatoBoram@users.noreply.github.com>
2023-12-06 21:28:26 -0500

    ✅ Play around with tests on durationAgo (#119)

--- src/lib/utils/dates.test.ts
+++ src/lib/utils/dates.test.ts
@@ -1,26 +1,42 @@
 import { describe, test } from 'vitest'
-import { fullTimeAgo, timeAgo } from './dates.js'
+import { durationAgo, fullTimeAgo, timeAgo } from './dates.js'
+import { Duration, durationUnit } from './duration.js'
 
-describe('timeAgo', () => {
-	test('just now', ({ expect }) => expect(timeAgo(new Date())).toBe('just now'))
+describe.concurrent('timeAgo', () => {
+	test('just now', ({ expect }) => {
+		const now = new Date()
+		return expect(timeAgo(now)).toBe('just now')
+	})
 
 	test('in the future', ({ expect }) => {
-		const date = new Date(8640000000000000)
-		expect(timeAgo(date)).toBe('in the future')
+		const maxDate = new Date(8640000000000000)
+		expect(timeAgo(maxDate)).toBe('in the future')
 	})
 
 	test('2 subepochs ago', ({ expect }) => {
-		const date = new Date(-8640000000000000)
-		expect(timeAgo(date)).toBe('2 subepochs ago')
+		const minDate = new Date(-8640000000000000)
+		expect(timeAgo(minDate)).toBe('2 subepochs ago')
 	})
 })
 
-describe('fullTime', () => {
-	test('min date', ({ expect }) => {
-		const date = new Date(-8640000000000000)
-		expect(fullTimeAgo(date)).toBeTypeOf('string')
+describe.concurrent('durationAgo', () => {
+	test('MAX_SAFE_INTEGER', ({ expect }) => {
+		const duration = new Duration(Number.MAX_SAFE_INTEGER, durationUnit.millisecond)
+		return expect(durationAgo(duration)).toBe('2 subepochs ago')
 	})
 
+	test('1 month ago', ({ expect }) => {
+		const duration = new Duration(1.1, durationUnit.month)
+		return expect(durationAgo(duration)).toBe('1 month ago')
+	})
+
+	test('2 months ago', ({ expect }) => {
+		const duration = new Duration(2.2, durationUnit.month)
+		return expect(durationAgo(duration)).toBe('2 months ago')
+	})
+})
+
+describe.concurrent('fullTime', () => {
 	test('in the future', ({ expect }) => {
 		const date = new Date(8640000000000000)
 		expect(fullTimeAgo(date)).toBe('in the future')
