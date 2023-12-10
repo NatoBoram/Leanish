4dde32cf012f7fe95bf3662c9f7a137671e11a9e
Nato Boram <nato.boram@gmail.com>
2023-12-09 23:00:46 -0500

    ✅ Add GitHub reporter to GitHub

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,5 +1,10 @@
 import type { PlaywrightTestConfig } from '@playwright/test'
 
+const reporter: PlaywrightTestConfig['reporter'] = [['html']]
+if (process.env.CI) reporter.push(['dot'])
+else reporter.push(['list'])
+if (process.env.GITHUB_ACTIONS) reporter.push(['github'])
+
 const config: PlaywrightTestConfig = {
 	webServer: {
 		command: 'pnpm run build && pnpm run preview',
@@ -7,6 +12,8 @@ const config: PlaywrightTestConfig = {
 	},
 	testDir: 'tests',
 	testMatch: /(.+\.)?(test|spec)\.[jt]s/,
+	reporter,
+	use: { browserName: 'firefox' },
 }
 
 export default config
