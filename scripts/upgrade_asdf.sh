c60a014f880eeabea63b751edff75f4e94bf5f5b
Nato Boram <NatoBoram@users.noreply.github.com>
2023-12-11 21:01:12 -0500

    ✨ IPFS build (#132)

--- scripts/upgrade_asdf.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-asdf install nodejs latest
-asdf install pnpm latest
-asdf local nodejs latest
-asdf local pnpm latest
--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,16 @@ import adapterStatic from '@sveltejs/adapter-static'
 import { vitePreprocess } from '@sveltejs/kit/vite'
 import { loadEnv } from 'vite'
 
-/** @type {{BUILD_ADAPTER: 'auto' | 'node' | 'static' | undefined, BUILD_BASE: string | undefined}} */
+/** @type {{BUILD_ADAPTER: 'auto' | 'node' | 'static', BUILD_BASE: string | undefined, BUILD_IPFS: 'false' | 'true'}} */
 const env = loadEnv(process.env.NODE_ENV ?? 'development', process.cwd(), 'BUILD_')
 
 /** @returns {import('@sveltejs/kit').Adapter} */
 function adapter() {
-	if (env.BUILD_ADAPTER === 'node') return adapterNode()
-	else if (env.BUILD_ADAPTER === 'static') return adapterStatic({ fallback: '404.html' })
-	else if (process.env.GITHUB_ACTIONS) return adapterStatic({ fallback: '404.html' })
+	if (env.BUILD_IPFS === 'true') return adapterStatic({ fallback: 'ipfs-404.html' })
+	else if (env.BUILD_ADAPTER === 'node') return adapterNode()
+	else if (env.BUILD_ADAPTER === 'static' || process.env.GITHUB_ACTIONS === 'true')
+		return adapterStatic({ fallback: '404.html' })
+
 	return adapterAuto()
 }
 
