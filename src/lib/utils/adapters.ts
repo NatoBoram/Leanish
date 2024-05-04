50e3c9bc89b855b6497ff21f76711ea6e3dee9b1
Nato Boram <nato.boram@gmail.com>
2024-05-04 15:24:07 -0400

    💡 Move @ts-expect-error

--- src/lib/utils/adapters.ts
+++ src/lib/utils/adapters.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 import type { LessThan, NumericRange } from '@sveltejs/kit'
 
-// @ts-expect-error Type instantiation is excessively deep and possibly infinite.
 export function isLessThan<Max extends number>(input: number, max: Max): input is LessThan<Max> {
 	return input < max
 }
@@ -15,7 +14,6 @@ export function isNumericRange<Min extends number, Max extends number>(
 }
 
 export function toLessThan<Max extends number>(input: number, max: Max): LessThan<Max> {
-	// @ts-expect-error Type instantiation is excessively deep and possibly infinite.
 	if (isLessThan(input, max)) return input
 	return (max - 1) as LessThan<Max>
 }
@@ -26,6 +24,7 @@ export function toNumericRange<Min extends number, Max extends number>(
 	max: Max,
 ): NumericRange<Min, Max> {
 	if (isLessThan(input, min)) return min as NumericRange<Min, Max>
+	// @ts-expect-error Type instantiation is excessively deep and possibly infinite.ts(2589)
 	if (isLessThan(input, max)) return input
 	return max as NumericRange<Min, Max>
 }
